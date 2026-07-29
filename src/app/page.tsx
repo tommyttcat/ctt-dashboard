e485eb8e144b93f0406239b1ca81a0e4796f6437
tommyttcat <99496284+tommyttcat@users.noreply.github.com>
2026-07-29 10:50:26 -0700

    Add Key Events card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import MarketSummary from '../components/MarketSummary';
 import TopMovers from '../components/TopMovers';
 import SwingCandidates from '../components/SwingCandidates';
 import Ep9m from '../components/Ep9m'; 
+import KeyEvents from '@/components/KeyEvents';
 
 export default function DailySetupsPage() {
   return (
@@ -38,6 +39,7 @@ export default function DailySetupsPage() {
             <div className="px-4 md:px-10 py-6 space-y-6">
               <Scorecard />
               <MarketSummary />
+              <KeyEvents />
               <TopMovers />
               <StocksInPlay />
               <DailySetupsComponent />
